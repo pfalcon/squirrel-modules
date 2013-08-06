a78386a2fd0cc7444f613351a82354eeb2a0eca6
Paul Sokolovsky <pfalcon@users.sourceforge.net>
2013-08-06 22:45:35 +0300

    Check that interpreter properly propagates script error to rc code.

--- import/test-import/run-tests.sh
+++ import/test-import/run-tests.sh
@@ -1,8 +1,14 @@
 #!/bin/sh
-set -e
 
 SQ=sqrl
 
+$SQ assert-check.nut
+if [ $? == 0 ]; then
+    echo "FAILED: Squirrel interpreter doesn't properly return script exit status"
+    exit 1
+fi
+
+set -e
 for t in test-*.nut; do
     echo $t
     $SQ $t
