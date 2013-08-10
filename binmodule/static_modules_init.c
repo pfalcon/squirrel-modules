64a3a9873e0b094c11b5dde5b43097e596adba2f
Paul Sokolovsky <pfalcon@users.sourceforge.net>
2013-08-10 22:11:33 +0300

    Automatically import module "stdlibinit" on startup.
    This module is supposed to import other import stdlib functionality, like
    high-level import. If import fails, issue warning.

--- binmodule/static_modules_init.c
+++ binmodule/static_modules_init.c
@@ -1,6 +1,17 @@
+#include <stdio.h>
 #include <sqimport.h>
 
 SQRESULT init_static_modules(HSQUIRRELVM v)
 {
-    return sqrat_register_importlib(v);
+    if (SQ_FAILED(sqrat_register_importlib(v)))
+        return SQ_ERROR;
+    sq_pushstring(v, _SC("stdlibinit"), -1);
+    sq_pushroottable(v);
+    if (SQ_FAILED(sq_import(v))) {
+        printf("WARNING: Error occurred while importing stdlibinit module:\n");
+        sq_pushroottable(v);
+        sq_getlasterror(v);
+        sqstd_aux_printerror(v);
+    }
+    return SQ_OK;
 }
